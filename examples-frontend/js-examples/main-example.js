e2f3962fcbd1c329ad9d172d484c9288dd1df9de
MerelyiGor <mogila1708@gmail.com>
2018-03-06 13:02:50 +0200

    Update main-example.js
    // Добавление класса при скролле

--- examples-frontend/js-examples/main-example.js
+++ examples-frontend/js-examples/main-example.js
@@ -1,1 +1,10 @@
-
+// Добавление класса при скролле 
+jQuery(window).scroll(function() {
+    var the_top = jQuery(document).scrollTop();
+    if (the_top > 100) {
+        jQuery('.top_mnu').addClass('fixed');
+    }
+    else {
+        jQuery('.top_mnu').removeClass('fixed');
+    }
+});
