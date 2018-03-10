c4cf5811dcf18b260f4e45a8530e8c71fb1a3643
MerelyiGor <mogila1708@gmail.com>
2018-03-10 08:22:16 +0200

    Create contact-form-7.php

---
+++ wp-examples/contact-form-7.php
@@ -0,0 +1,4 @@
+<?php/**
+********   вывод шорткода с контактной формой из плагина (contact form 7)
+****/?>
+<?php echo do_shortcode('[contact-form-7 id="193" title="form-send-order-hackintosh"]'); ?>
