1d858692affdc52b16ceab7df8208c6bce2bf099
MerelyiGor <mogila1708@gmail.com>
2018-03-10 08:23:04 +0200

    Update contact-form-7.php
    вывод шорткода с контактной формой из плагина (contact form 7)

--- wp-examples/contact-form-7.php
+++ wp-examples/contact-form-7.php
@@ -1,4 +1,4 @@
 <?php/**
 ********   вывод шорткода с контактной формой из плагина (contact form 7)
 ****/?>
-<?php echo do_shortcode('[contact-form-7 id="193" title="form-send-order-hackintosh"]'); ?>
+<?php echo do_shortcode('[contact-form-7 id="228" title="контактная форма 1"]'); ?>
