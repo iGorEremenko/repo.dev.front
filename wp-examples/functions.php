672a63e50e7d7cb6ec05dd3a0e8c905602fa88d4
MerelyiGor <mogila1708@gmail.com>
2018-05-02 22:44:35 +0300

    Update functions.php
    добавил функции к вп нав меню

--- wp-examples/functions.php
+++ wp-examples/functions.php
@@ -81,13 +81,29 @@ add_action('wp_print_styles', 'название_темы');
 /**
  *   убираем контейнер меню
  */
+register_nav_menus(array(
+	'main-menu'    => 'Main Menu',    //Название месторасположения меню в шаблоне
+	'container' => 'div', // контейнер для меню, по умолчанию 'div', в нашем случае ставим 'nav', пустая строка - нет контейнера
+	'container_class' => 'collapse navbar-collapse', // класс для контейнера
+	'container_id' => 'collapse-1', // id для контейнера
+	'menu_class' => 'nav navbar-nav', // класс для меню
+	'menu_id' => '', // id для меню
+));
+
 add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args');
-function my_wp_nav_menu_args($args = '')
+function my_wp_nav_menu_args($args)
 {
-    $args['container'] = false;
-    return $args;
+	$args['container'] = false; //убираем контейнер
+	return $args;
 }
 
+// убираю классы и id элементов меню
+function my_remove_all_class_item($classes) {
+	$classes = '';
+	return $classes;
+}
+add_filter('nav_menu_css_class', 'my_remove_all_class_item', 10, 2 ); // убираем классы элементов меню
+add_filter('nav_menu_item_id', '__return_false'); // убираем id элементов меню
 
 /**
  *   функция тега more... превью записи на главной
